fb5574fcc3ea7004cc8df1d310f43cedb8c645fa
Russell Gilmore <RussellGilmore@live.com>
2026-03-27 10:17:39 -0400

    Route to NAT Gateways when not using centralized NAT

--- red-network/private.tf
+++ red-network/private.tf
@@ -22,3 +22,27 @@ resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.subnets[each.key].id
   route_table_id = aws_route_table.private.id
 }
+
+####################################################################################################
+# Private Subnet Default Routes
+####################################################################################################
+
+# Route to NAT Gateway for private subnets (when NOT using centralized NAT)
+resource "aws_route" "private_nat" {
+  count = local.create_nat_gateway ? 1 : 0
+
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.main[0].id
+}
+
+# Route to Transit Gateway for private subnets (when using centralized NAT)
+# This sends all internet-bound traffic from private subnets through the TGW
+# to the hub VPC where the shared NAT gateway provides outbound access
+resource "aws_route" "private_centralized_nat" {
+  count = var.use_centralized_nat && local.effective_attach_to_tgw ? 1 : 0
+
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  transit_gateway_id     = local.effective_transit_gateway_id
+}
